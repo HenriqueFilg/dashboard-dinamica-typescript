597713662047cf5b8c46fbcc67ce06c1548781b4
Henriquefilg <henriquefilg.silva@gmail.com>
2026-06-01 21:10:58 -0300

    Adicionando  Children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import Menu from "../components/Menu";
 import Footer from "../components/Menu/Footer";
+import User from "../components/User";
 
 const Home = () => {
+
+  const userName = "Cesar";
   return (
     <div>
       <Menu /><br />
+
+      <User name={userName}>
+          <p>Este é um conteúdo extra fornecido como children.</p>
+      </User>
       <h2>Bem vindo Henrique!</h2><br />
       <Footer />
     </div>
---
+++ src/components/User/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface UserProps {
+    name: string;
+    children?: ReactNode
+}
+
+const User = ({ name, children }: UserProps) => {
+    return(
+        <div>
+            <p>Usuário: {name}</p>
+            {children && <div>{children}</div>}
+        </div>
+    )
+}
+
+export default User;
