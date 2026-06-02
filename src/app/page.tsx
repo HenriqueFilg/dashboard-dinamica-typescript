f427c43fd7e4efa592305c0422540598e331f527
Henriquefilg <henriquefilg.silva@gmail.com>
2026-06-01 21:34:18 -0300

    Adicionando Hooks e useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
+'use client'
+import { useState } from "react";
 import Menu from "../components/Menu";
 import Footer from "../components/Menu/Footer";
 import User from "../components/User";
 
 const Home = () => {
 
-  const userName = "Cesar";
+  const [nameUser, setNameUser] = useState ("Cesar");
   return (
     <div>
       <Menu /><br />
 
-      <User name={userName}>
+      <User name={nameUser}>
           <p>Este é um conteúdo extra fornecido como children.</p>
-      </User>
+      </User><br />
+
+      <button onClick={() => setNameUser("Vinicius")}>Alterar</button>
+
       <h2>Bem vindo Henrique!</h2><br />
       <Footer />
     </div>
